ac2688931e6d7eb43289999edfd7f7b2ddc16965
Chris Ermel <ermel272@gmail.com>
2017-12-05 01:25:28 -0500

    set up to add algorithms

---
+++ js/algorithms.js
@@ -0,0 +1,2 @@
+function giftWrap(vertices) {
+}
--- js/graph.js
+++ js/graph.js
@@ -3,6 +3,9 @@ var radius = 5;
 var two_params = { width: 800, height: 600 };
 var two = null;
 var vertices = [];
+var algorithms = {
+    "gift_wrap": giftWrap
+}
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -14,10 +17,10 @@ function getRandomInt(min, max) {
 function createVertices() {
 		num = document.getElementById('num_vertices').value;
 		
-		if (num === "") {
-			return
-		}
+		if (num === "") { return }
 		
+        // Unbind update event and clear canvas
+        two.unbind('update', null)
 		two.clear()
 		vertices = []
 		
@@ -32,6 +35,7 @@ function createVertices() {
 		var group = two.makeGroup(...vertices);
 		group.scale = 0.0;
 		
+		// Animate the creation of the vertices
 		two.bind('update', function(frameCount) {
 		  if (group.scale < 0.9999) {
 			var t = (1 - group.scale) * 0.125;
@@ -40,22 +44,21 @@ function createVertices() {
 		}).play();
 }
 
-// Load canvas once DOM has been loaded
+function execute() {
+    if (vertices.length === 0) { return }
+    
+    // Find & execute algorithm chosen in the DOM
+    alg = document.getElementById('algorithm').value;
+    algorithms[alg](vertices)
+}
+
+// Create canvas once DOM has been loaded
 document.addEventListener("DOMContentLoaded", function(event) { 
 	// Place canvas on the page
-	var elem = document.getElementById('canvas');
-	two = new Two(two_params).appendTo(elem);
+	var canvas = document.getElementById('canvas');
+	two = new Two(two_params).appendTo(canvas);
 
-	var circle = two.makeCircle(72, 100, 50);
-	var rect = two.makeRectangle(213, 100, 100, 100);
-
-	circle.fill = '#FF8000';
-	circle.stroke = 'orangered';
-	circle.linewidth = 5;
-
-	rect.fill = 'rgb(0, 200, 255)';
-	rect.opacity = 0.75;
-	rect.noStroke();
+	two.makeText("Create vertices to start", two_params.width/2, two_params.height/2);
 
 	// Render to the canvas
 	two.update();
